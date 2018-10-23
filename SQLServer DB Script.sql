914aac7e8ddee5e13774891d4567d99079b06cbd
Steve <262877418@qq.com>
2018-10-23 16:50:34 +0800

    change month

--- SQLServer DB Script.sql
+++ SQLServer DB Script.sql
@@ -1,6 +1,6 @@
 ﻿USE [WebAPIDB]
 GO
-/****** Object:  Table [dbo].[Employee]    Script Date: 01/01/2018 7:43:19 PM ******/
+/****** Object:  Table [dbo].[Employee]    Script Date: 12/01/2018 7:43:19 PM ******/
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Employee]') AND type in (N'U'))
 DROP TABLE [dbo].[Employee]
 GO
